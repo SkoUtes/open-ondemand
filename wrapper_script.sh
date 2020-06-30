c7b054b944a25d01c46cf6b747d10c589bc0aff2
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2020-06-30 13:03:44 -0600

    Update wrapper_script.sh

--- wrapper_script.sh
+++ wrapper_script.sh
@@ -2,22 +2,5 @@
 
 # Start up Ruby
 scl enable rh-ruby25 bash
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start Ruby 2.5: $status"
-  exit $status
-elif [[ $status = 0 ]]; then
-	echo "rh-ruby25 enabled"
-	exit $status
-fi
-
 # Start up Node.js
 scl enable rh-nodejs10 bash
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start Node.js 10: $status"
-  exit $status
-elif [[ $status = 0 ]]; then
-	echo "rh-nodejs10 enabled"
-	exit $status
-fi
