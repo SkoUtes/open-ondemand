f505628ca6a151070b7950dc9d374cdfc3ec7423
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2020-07-02 11:09:39 -0600

    Trying out environment variables for running scripts

--- nodejs.sh
+++ nodejs.sh
@@ -5,6 +5,7 @@ echo "nodejs.sh is running now"
 echo "===================================="
 echo "===================================="
 # Enable nodejs10
+unset BASH_ENV PROMPT_COMMAND ENV
 scl enable rh-nodejs10 bash
 status=$?
 if [ $status -ne 0 ]; then
--- ruby.sh
+++ ruby.sh
@@ -5,6 +5,7 @@ echo "ruby.sh is running now"
 echo "===================================="
 echo "===================================="
 # Enable nodejs10
+unset BASH_ENV PROMPT_COMMAND ENV
 scl enable rh-ruby25 bash
 status=$?
 if [ $status -ne 0 ]; then
