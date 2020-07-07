f3b418d9b238e7cb98acd3905b47684a307b5fc8
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2020-07-07 14:00:37 -0600

    Update ruby-node.sh

--- ruby-node.sh
+++ ruby-node.sh
@@ -8,8 +8,7 @@ echo "===================================="
 set ENV BASH_ENV="/root/scripts/ruby-node.sh" \
     ENV="/root/scripts/ruby-node.sh" \
     PROMPT_COMMAND=". /root/scripts/ruby-node.sh"
-scl enable rh-ruby25 bash
-scl enable rh-nodejs10 bash
+scl enable rh-ruby25 rh-nodejs10 bash
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start ruby and node.js: $status"
