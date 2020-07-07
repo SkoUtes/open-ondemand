72c22155e576714da6e750ad2f866e264d6aafab
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2020-07-07 10:48:59 -0600

    Trying to change runtime priority

--- ruby-node.sh
+++ ruby-node.sh
@@ -5,10 +5,12 @@ echo "rh-ruby25 and nodejs10 are now running"
 echo "===================================="
 echo "===================================="
 # Enable nodejs10
-scl enable rh-ruby25 rh-nodejs10 bash
+scl enable rh-ruby25 bash
+scl enable rh-nodejs10 bash
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start ruby and node.js: $status"
   exit $status
 fi
 unset BASH_ENV PROMPT_COMMAND ENV
+supervisorctl start apache
