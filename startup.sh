667ae13847836489f6c8de11467cb134de26c054
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2020-10-13 09:18:12 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 sleep 60
-## Print out island container Service IP and public keys into ssh_known_hosts file
-{ echo -n "open-ondemand,"; \
-printenv -0 $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_CLUSTER_IP_SERVICE_HOST") && \
-cat /etc/ssh/ssh_host_ecdsa_key.pub ;} >> /etc/ssh/ssh_known_hosts
-{ echo -n "open-ondemand,"; \
-printenv -0 $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_CLUSTER_IP_SERVICE_HOST") && \
-cat /etc/ssh/ssh_host_ed25519_key.pub ;} >> /etc/ssh/ssh_known_hosts
-{ echo -n "open-ondemand,"; \
-printenv -0 $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_CLUSTER_IP_SERVICE_HOST") && \
-cat /etc/ssh/ssh_host_rsa_key.pub ;} >> /etc/ssh/ssh_known_hosts
+## Print out Pod IPs and node names into the /etc/hosts file
+{ printenv -0 $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_ISLAND_SERVICE_HOST") && \
+echo -n "  ood-island" ;} >> /etc/hosts
+{ printenv -0 $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_CLUSTER_IP_SERVICE_HOST") && \
+echo -n "  open-ondemand" ;} >> /etc/hosts
 
 ## Print out ondemand container Service IP and public keys into ssh_known_hosts file
 { echo -n "ondemand-island," && \
@@ -21,6 +16,17 @@ $(printenv $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && e
 { echo -n "ondemand-island," && \
 ssh-keyscan -t rsa 2> /dev/null \
 $(printenv $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_ISLAND_SERVICE_HOST")) ;} >> /etc/ssh/ssh_known_hosts
+
+## Print out island container Service IP and public keys into ssh_known_hosts file
+{ echo -n "open-ondemand,"; \
+printenv -0 $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_CLUSTER_IP_SERVICE_HOST") && \
+cat /etc/ssh/ssh_host_ecdsa_key.pub ;} >> /etc/ssh/ssh_known_hosts
+{ echo -n "open-ondemand,"; \
+printenv -0 $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_CLUSTER_IP_SERVICE_HOST") && \
+cat /etc/ssh/ssh_host_ed25519_key.pub ;} >> /etc/ssh/ssh_known_hosts
+{ echo -n "open-ondemand,"; \
+printenv -0 $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_CLUSTER_IP_SERVICE_HOST") && \
+cat /etc/ssh/ssh_host_rsa_key.pub ;} >> /etc/ssh/ssh_known_hosts
 
 ## Disable SSHD
 sleep 60
