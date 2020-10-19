e1f9c706c75c69f9902d2d0d9ea58835be97359c
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2020-10-19 14:29:00 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -27,15 +27,15 @@ ssh-keyscan -t rsa 2> /dev/null \
 $(printenv $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_ISLAND_SERVICE_HOST")) ;} >> /etc/ssh/ssh_known_hosts
 
 ## Print out island container Service IP and public keys into ssh_known_hosts file
-{ echo -n "$INSTANCE_NAME-cluster-ip," | tr -d ' ' && \
+{ echo -n "$INSTANCE_NAME-cluster-ip," && \
 printenv -0 $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_CLUSTER_IP_SERVICE_HOST") && \
-cat /etc/ssh/ssh_host_ecdsa_key.pub ;} >> /etc/ssh/ssh_known_hosts
-{ echo -n "$INSTANCE_NAME-cluster-ip," | tr -d ' ' && \
+echo -n " " && cat /etc/ssh/ssh_host_ecdsa_key.pub ;} >> /etc/ssh/ssh_known_hosts
+{ echo -n "$INSTANCE_NAME-cluster-ip," && \
 printenv -0 $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_CLUSTER_IP_SERVICE_HOST") && \
-cat /etc/ssh/ssh_host_ed25519_key.pub ;} >> /etc/ssh/ssh_known_hosts
-{ echo -n "$INSTANCE_NAME-cluster-ip," | tr -d ' ' && \
+echo -n " " && cat /etc/ssh/ssh_host_ed25519_key.pub ;} >> /etc/ssh/ssh_known_hosts
+{ echo -n "$INSTANCE_NAME-cluster-ip," && \
 printenv -0 $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_CLUSTER_IP_SERVICE_HOST") && \
-cat /etc/ssh/ssh_host_rsa_key.pub ;} >> /etc/ssh/ssh_known_hosts
+echo -n " " && cat /etc/ssh/ssh_host_rsa_key.pub ;} >> /etc/ssh/ssh_known_hosts
 
 ## Disable SSHD
 # sleep 60
