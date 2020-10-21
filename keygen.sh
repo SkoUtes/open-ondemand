2d02957a8c28f6723e6edffb8d365b09f0526278
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2020-10-21 11:16:48 -0600

    adding dsa keys back in

--- keygen.sh
+++ keygen.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-ssh-keygen -A && chmod 600 /etc/ssh/* && \
-rm /etc/ssh/ssh_host_dsa_key /etc/ssh/ssh_host_dsa_key.pub
+ssh-keygen -A && chmod 600 /etc/ssh/*
--- startup.sh
+++ startup.sh
@@ -17,6 +17,9 @@ printenv $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && ech
 
 ## Print out island container Service IP and public keys into ssh_known_hosts file
 { echo -n "$INSTANCE_NAME-island," && \
+ssh-keyscan -t dsa 2> /dev/null \
+$(printenv $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_ISLAND_SERVICE_HOST")) ;} >> /etc/ssh/ssh_known_hosts
+{ echo -n "$INSTANCE_NAME-island," && \
 ssh-keyscan -t ecdsa 2> /dev/null \
 $(printenv $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_ISLAND_SERVICE_HOST")) ;} >> /etc/ssh/ssh_known_hosts
 { echo -n "$INSTANCE_NAME-island," && \
@@ -27,6 +30,9 @@ ssh-keyscan -t rsa 2> /dev/null \
 $(printenv $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_ISLAND_SERVICE_HOST")) ;} >> /etc/ssh/ssh_known_hosts
 
 ## Print out ondemand container Service IP and public keys into ssh_known_hosts file
+{ echo -n "$INSTANCE_NAME-cluster-ip," && \
+printenv -0 $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_CLUSTER_IP_SERVICE_HOST") && \
+cat /etc/ssh/ssh_host_dsa_key.pub ;} >> /etc/ssh/ssh_known_hosts
 { echo -n "$INSTANCE_NAME-cluster-ip," && \
 printenv -0 $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_CLUSTER_IP_SERVICE_HOST") && \
 cat /etc/ssh/ssh_host_ecdsa_key.pub ;} >> /etc/ssh/ssh_known_hosts
