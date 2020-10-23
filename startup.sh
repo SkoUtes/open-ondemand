2e4fd0ea5f612af3144c9fd6b4c8b013a6f29f27
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2020-10-23 12:23:54 -0600

    made a mistake deleting the wrong code

--- startup.sh
+++ startup.sh
@@ -15,19 +15,19 @@ printenv $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && ech
 { echo -n "$INSTANCE_NAME" "-cluster-ip" | tr -d ' ' && echo " +" && \ 
 printenv $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_CLUSTER_IP_SERVICE_HOST") ;} >> /etc/shosts.equiv
 
-## Print out island container Service IP and public keys into ssh_known_hosts file
-{ echo -n "$INSTANCE_NAME-island," && \
-ssh-keyscan -t dsa 2> /dev/null \
-$(printenv $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_ISLAND_SERVICE_HOST")) ;} >> /etc/ssh/ssh_known_hosts
-{ echo -n "$INSTANCE_NAME-island," && \
-ssh-keyscan -t ecdsa 2> /dev/null \
-$(printenv $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_ISLAND_SERVICE_HOST")) ;} >> /etc/ssh/ssh_known_hosts
-{ echo -n "$INSTANCE_NAME-island," && \
-ssh-keyscan -t ed25519 2> /dev/null \
-$(printenv $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_ISLAND_SERVICE_HOST")) ;} >> /etc/ssh/ssh_known_hosts
-{ echo -n "$INSTANCE_NAME-island," && \
-ssh-keyscan -t rsa 2> /dev/null \
-$(printenv $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_ISLAND_SERVICE_HOST")) ;} >> /etc/ssh/ssh_known_hosts
+## Print out ondemand container Service IP and public keys into ssh_known_hosts file
+{ echo -n "$INSTANCE_NAME-cluster-ip," && \
+printenv -0 $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_CLUSTER_IP_SERVICE_HOST") && \
+cat /etc/ssh/ssh_host_dsa_key.pub ;} >> /etc/ssh/ssh_known_hosts
+{ echo -n "$INSTANCE_NAME-cluster-ip," && \
+printenv -0 $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_CLUSTER_IP_SERVICE_HOST") && \
+cat /etc/ssh/ssh_host_ecdsa_key.pub ;} >> /etc/ssh/ssh_known_hosts
+{ echo -n "$INSTANCE_NAME-cluster-ip," && \
+printenv -0 $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_CLUSTER_IP_SERVICE_HOST") && \
+cat /etc/ssh/ssh_host_ed25519_key.pub ;} >> /etc/ssh/ssh_known_hosts
+{ echo -n "$INSTANCE_NAME-cluster-ip," && \
+printenv -0 $(echo -n "$INSTANCE_NAME" | tr [:lower:] [:upper:] | tr '-' '_' && echo -n "_CLUSTER_IP_SERVICE_HOST") && \
+cat /etc/ssh/ssh_host_rsa_key.pub ;} >> /etc/ssh/ssh_known_hosts
 
 ## Disable SSHD
 # sleep 60
