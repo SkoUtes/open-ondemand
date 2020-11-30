9eaec748428415fa020c915dd4a382f242cee564
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2020-11-30 11:41:36 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 scl enable ondemand /var/www/ood/apps/sys/shell/bin/setup
+mkdir /etc/ood/config/apps /etc/ood/config/apps/shell
+touch /etc/ood/config/apps /etc/ood/config/apps/shell/env
+cat <<EOF > /etc/ood/config/apps /etc/ood/config/apps/shell/env
+OOD_SSHHOST_ALLOWLIST=""
+OOD_SHELL_ORIGIN_CHECK="off"
+EOF
