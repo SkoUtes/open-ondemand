c0c8e14f301806e6837cb4acf86cfee360781558
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2021-01-11 15:11:44 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,7 +7,6 @@ OOD_SSHHOST_ALLOWLIST=""
 OOD_SHELL_ORIGIN_CHECK="off"
 EOF
 sleep 10
-./auth_openidc.sh
 chgrp apache /opt/rh/httpd24/root/etc/httpd/conf.d/auth_openidc.conf
 chmod 640 /opt/rh/httpd24/root/etc/httpd/conf.d/auth_openidc.conf
 sudo /opt/ood/ood-portal-generator/sbin/update_ood_portal
