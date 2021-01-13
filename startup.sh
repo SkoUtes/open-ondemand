23ef552f12150f2e7aab9f6bf984115eec687ba2
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2021-01-13 11:26:17 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,6 +7,25 @@ OOD_SSHHOST_ALLOWLIST=""
 OOD_SHELL_ORIGIN_CHECK="off"
 EOF
 sleep 20
+printf "
+    OIDCProviderMetadataURL https://$(echo $INSTANCE_NAME).keycloak.$(echo $CLUSTER_DNS)/auth/realms/ondemand/.well-known/openid-configuration\n\
+    OIDCClientID        \"`cat /shared/id`\" \n\
+    OIDCClientSecret    \"`cat /shared/client-secret`\"\n\
+    OIDCRedirectURI      https://$(echo $INSTANCE_NAME).ondemand.$(echo $CLUSTER_DNS)/oidc\n\
+    OIDCCryptoPassphrase 'd14e5b4c8e6257ea81830f23c2be4633ad04d3af9816affdda6d8fec8ea926a000ca47b507587dc5'\n\
+    \n\
+    $(echo '# Keep sessions alive for 8 hours')\n\
+    OIDCSessionInactivityTimeout 28800\n\
+    OIDCSessionMaxDuration 28800\n\
+    \n\
+    $(echo '# Set REMOTE_USER')\n\
+    OIDCRemoteUserClaim preferred_username\n\
+    \n\
+    $(echo '# Do not pass claims to backend servers')\n\
+    OIDCPassClaimsAs environment\n\
+    \n\
+    $(echo '# Strip out session cookies before passing to backend')\n\
+    OIDCStripCookies mod_auth_openidc_session mod_auth_openidc_session_chunks mod_auth_openidc_session_0 mod_auth_openidc_session_1 " > '/opt/rh/httpd24/root/etc/httpd/conf.d/auth_openidc.conf'
 chgrp apache /opt/rh/httpd24/root/etc/httpd/conf.d/auth_openidc.conf
 chmod 640 /opt/rh/httpd24/root/etc/httpd/conf.d/auth_openidc.conf
 sudo /opt/ood/ood-portal-generator/sbin/update_ood_portal
